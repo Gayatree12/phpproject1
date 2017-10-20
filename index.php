c1af741a34bbeb1c9b41a0ced0941d517b2d8ffa
Gayatree Tamuli <gt86@njit.edu>
2017-10-19 23:14:32 -0400

    errorReport&autoloader added

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+//to output error messages
+ini_set('display_errors', 'On');
+error_reporting(E_ALL); //report all PHP errors; E_ALL is a constant
+
+class manageFiles {
+	public function __construct() {
+		$include . '.php';
+	}
+spl_autoload_register();  //register multiple autoloader functions
+}
+
+?>
