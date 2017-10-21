6303b4111c956a9bca0c529993b1d7b3b6f98499
Gayatree Tamuli <gt86@njit.edu>
2017-10-21 14:01:04 -0400

    page request

--- index.php
+++ index.php
@@ -7,7 +7,21 @@ class manageFiles {
 	public function __construct() {
 		$include . '.php';
 	}
-spl_autoload_register();  //register multiple autoloader functions
+  spl_autoload_register(array('manageFiles','autoload'));  //register multiple autoloader functions
 }
-
+class main {
+  public function __construct() {
+  $pagerequest = 'uploadform';
+    if(isset($_REQUEST['page'])) {
+      $pagerequest = $_REQUEST['page'];
+    }
+    $page = new $pagerequest; //instantiation
+    if($_SERVER['REQUEST_METHOD'] == GET) {
+    $page -> get();
+    }
+      else {
+        $page -> post();
+      }  
+  } 
+}
 ?>
