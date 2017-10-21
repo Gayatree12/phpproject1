8d53206ac61780df9be0ad7e077fbda48f246ad3
Gayatree Tamuli <gt86@njit.edu>
2017-10-21 19:00:36 -0400

    errors removed

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
 //to output error messages
 ini_set('display_errors', 'On');
-error_reporting(E_ALL); //report all PHP errors; E_ALL is a constant
+//error_reporting(E_ALL); //report all PHP errors; E_ALL is a constant
 
 class manageFiles {
-	public function __construct() {
-		$include . '.php';
+	public static function autoload($class) {
+		include $class . '.php';
 	}
-  spl_autoload_register(array('manageFiles','autoload'));  //register multiple autoloader functions
 }
+spl_autoload_register(array('manageFiles','autoload'));  //register multiple autoloader functions
+$obj = new main(); 
 class main {
   public function __construct() {
   $pagerequest = 'uploadform';
@@ -16,12 +17,23 @@ class main {
       $pagerequest = $_REQUEST['page'];
     }
     $page = new $pagerequest; //instantiation
-    if($_SERVER['REQUEST_METHOD'] == GET) {
+    if($_SERVER['REQUEST_METHOD'] == 'GET') {
     $page -> get();
     }
       else {
         $page -> post();
       }  
   } 
+}
+abstract class page {
+  protected $myFile;
+  public function __construct() {
+    $this -> myFile .= '<html></body>';
+    $this -> myFile .= '<link rel="stylesheet" href="styles.css">';
+  }
+  public function __destruct() {
+    $this -> myFile .= '</html></body>';
+    print($this -> myFile);
+  }
 }
 ?>
