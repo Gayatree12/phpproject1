14888185b8b5ef9da948cdba851d894b5e7fe71c
Gayatree Tamuli <gt86@njit.edu>
2017-10-21 22:02:56 -0400

    post function added

--- uploadform.php
+++ uploadform.php
@@ -1,5 +1,5 @@
 <?php
-
+//creating html form
 class uploadform extends page {
   public function get() {
     $this -> myFile .= '<h1>Web Systems Development: IS601</h1>';
@@ -12,4 +12,10 @@ class uploadform extends page {
     $this -> myFile .= $form;
   }
   
+  public function post() {
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["uploadfile"]["name"]);
+    move_uploaded_file($_FILES["uploadfile"]["tmp_name"], $target_file);
+    header('Location: index.php?page=tabledisplay&filename=' . $target_file);
+  }
 }
